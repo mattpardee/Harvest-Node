006689ecb8c3f3ab834735ed8efbabfbae89294c
Matt Pardee <matt.pardee@gmail.com>
2011-01-10 03:29:08 -0800

    Source

--- harvestapi.js
+++ harvestapi.js
@@ -1,5 +1,7 @@
-var sys  = require('sys'),
-    rest = require('./res/restler/lib/restler.js');
+var sys    = require('sys'),
+    qs     = require('querystring'),
+    xml2js = require('./res/node-xml2js/lib/'),
+    rest   = require('./res/restler/lib/restler.js');
 
 HarvestREST = rest.service(
 	function(u, p) {
@@ -9,22 +11,38 @@ HarvestREST = rest.service(
 	, {}
 	, {
 		runOp: function(type, url, data) {
-			data.headers = { 'Content-Type': 'application/json', 'Accept': 'application/json' };
+			var opts = {};
+			opts.headers = { 'Content-Type': 'application/xml', 'Accept': 'application/xml' };
+    		if(typeof data !== 'undefined') {
+        		if(typeof data === 'object') {
+            	    opts.headers['Content-Length'] = qs.stringify(data).length;
+                }
+                
+                else {
+                    opts.headers['Content-Length'] = data.length;
+                }
+    		}
+        	
+            else {
+                opts.headers['Content-Length'] = 0;   
+            }
+
+			opts.data = data;
 			switch(type) {
 				case 'get':
-					return this.get(url, data);
+					return this.get(url, opts);
 					break;
 					
 				case 'post':
-					return this.post(url, data);
+					return this.post(url, opts);
 					break;
 					
 				case 'put':
-					return this.put(url, data);
+					return this.put(url, opts);
 					break;
 					
 				case 'delete':
-					return this.del(url, data);
+					return this.del(url, opts);
 					break;
 			}
 		}
@@ -37,9 +55,22 @@ var Harvest = this.Harvest = function(username, password, harvest_subdomain) {
 	
 	this.procResults = function(res, cb) {
 		res.addListener('success', function(data) {
-			cb('success', data);
+    	    // Trim the data
+    	    data = data.replace(/^\s*|\s*$/, '');
+    	    if(data != '') {
+    	        parser = new xml2js.Parser();
+                parser.addListener('end', function(result) {
+                    cb('success', result);        
+                });
+                parser.parseString(data);
+    	    }
+            
+            else {
+                cb('success', data);   
+            }
 		}).addListener('error', function(data) {
 			cb('error', data);
+			//sys.puts(sys.inspect(res));
 		});
 	};
 	
@@ -53,12 +84,103 @@ var Harvest = this.Harvest = function(username, password, harvest_subdomain) {
 			cb = day;
 		}
 		
-		this.procResults(this.client.runOp('get', turl, {}), cb);
+		this.procResults(this.client.runOp('get', turl), cb);
 	};
+    
+    this.getSingleEntry = function(day_entry_id, cb) {
+        this.procResults(this.client.runOp('get', '/daily/show/' + day_entry_id), cb);
+    };
 	
 	this.toggleTimer = function(entry_id, cb) {
-		this.procResults(this.client.runOp('get', '/daily/timer/' + entry_id, {}), cb);
+		this.procResults(this.client.runOp('get', '/daily/timer/' + entry_id), cb);
 	};
+	
+	this.newDailyEntry = function(project_id, task_id, date_logging, notes, hours, cb) {
+        xmlstr = '<request><notes>' + notes + '</notes><hours>' + hours 
+                    + '</hours><project_id type="integer">' + project_id
+                    + '</project_id><task_id type="integer">' + task_id + '</task_id>'
+                    + '<spent_at type="date">' + date_logging + '</spent_at>'
+                    + '</request>';
+		this.procResults(
+			this.client.runOp('post', '/daily/add', xmlstr) 
+			, cb
+		);
+	};
+    
+    this.deleteEntry = function(day_entry_id, cb) {
+        this.procResults(this.client.runOp('delete', '/daily/delete/' + day_entry_id), cb);
+    };
+    
+    this.updateEntry = function(day_entry_id, project_id, task_id, date_logged, notes, hours, cb) {
+        xmlstr = '<request><notes>' + notes + '</notes><hours>' + hours + '</hours>'
+                    + '<spent_at type="date">' + date_logged + '</spent_at>'
+                    + '<project_id type="integer">' + project_id + '</project_id>'
+                    + '<task_id type="integer">' + task_id + '</task_id>'
+                    + '</request>';
+        this.procResults(
+        	this.client.runOp('post', '/daily/update/' + day_entry_id, xmlstr)
+            , cb
+        );
+    };
+    
+    /*****
+     * getClients()
+     * 
+     * Gets a list of all clients
+     * 
+     *****/
+    this.getClients = function(cb) {
+        this.procResults(
+            this.client.runOp('get', '/clients')
+            , cb
+        );
+    };
+
+    /*****
+    * getSingularClient()
+    * 
+    * Gets details about a single client
+    * 
+    *****/
+    this.getSingularClient = function(client_id, cb) {
+        this.procResults(
+            this.client.runOp('get', '/clients/' + client_id)
+            , cb
+        );
+    };
+    
+    /*****
+    * createNewClient()
+    * 
+    * Creates a new client
+    * 
+    * client_name is required, and is the company name or a person's name
+    * details are not required, but would be address, phone, website, etc
+    * 
+    * This HTTP response is 201 with the new client ID in this style:
+    * /clients/#{new_client_id}
+    * 
+    *****/
+    this.createNewClient = function(client_name, details, cb) {
+        xmlstr = '<client>'
+                + '<name>' + client_name + '</name>';
+        
+        if(typeof details !== 'function') {
+            xmlstr += '<details>' + details + '</details>';
+        }
+        
+        xmlstr += '</client>';
+        
+        // Instead of having procresults call the callback, I think we should either:
+        // 1. Handle it ourselves since this is a 201, parse out the new client ID
+        //    and send it to the client as JSON, or
+        // 2. Handle 201 events in procResults if there is consistency in
+        //    the Harvest API with regards to how resultant URLs are returned
+        this.procResults(
+            this.client.runOp('post', '/clients/', xmlstr)
+            , cb
+        );
+    };
 };
 
 var _exports = { Harvest: Harvest };
